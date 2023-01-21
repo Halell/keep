ce6c8c465210505e5cb9032cdb8f5314b49d7e0d
Halell <halellhofman@gmail.com>
2023-01-21 19:06:41 +0200

    bug fix in notification

--- src/cmp/notes.jsx
+++ src/cmp/notes.jsx
@@ -14,61 +14,61 @@ import { ReactComponent as AddImgIco } from '../assets/svg/add-img-ico.svg'
 // class for notes
 class Notes extends Component {
 
-    state =
+    state = {
         bgClr: this.props.note.bgClr,
-            isClrPickerOpen: false,
-                deleteModal: false
+        isClrPickerOpen: false,
+        deleteModal: false
     }
-//need to go to the handlingNote HOC 
-setBgClr = (bgClr) => {
-    this.setState({ bgClr: bgClr })
-    let note = this.props.note
-    note.bgClr = bgClr
-    Service.saveNote(note)
-}
-ClrPikerOpen = (isOpen) => {
-    this.setState({ isClrPickerOpen: isOpen })
-}
+    //need to go to the handlingNote HOC 
+    setBgClr = (bgClr) => {
+        this.setState({ bgClr: bgClr })
+        let note = this.props.note
+        note.bgClr = bgClr
+        Service.saveNote(note)
+    }
+    ClrPikerOpen = (isOpen) => {
+        this.setState({ isClrPickerOpen: isOpen })
+    }
 
-render() {
-    const { note } = this.props
-    const { isClrPickerOpen } = this.state
-    const style = { '--bgClr': this.state.bgClr, '--clr': `${(this.state.bgClr === '#ffffff') ? '#000000' : '#d4f2ff'}` }
-    return (
-        <section className="note" style={ { zIndex: this.props.zIndex } }>
-            <div className={ `container bg-clr-opt ` } style={ style } >
-                <div className="title-container">
-                    <div className="title" >{ `${note.title}` } </div>
-                    <div className="icons btn-pin-icon"></div>
+    render() {
+        const { note } = this.props
+        const { isClrPickerOpen } = this.state
+        const style = { '--bgClr': this.state.bgClr, '--clr': `${(this.state.bgClr === '#ffffff') ? '#000000' : '#d4f2ff'}` }
+        return (
+            <section className="note" style={ { zIndex: this.props.zIndex } }>
+                <div className={ `container bg-clr-opt ` } style={ style } >
+                    <div className="title-container">
+                        <div className="title" >{ `${note.title}` } </div>
+                        <div className="icons btn-pin-icon"></div>
+                    </div>
+                    <div className={ "body" }>
+                        { note.body.split("\n").map((line, index) => (
+                            <Fragment key={ index }>
+                                { line }
+                                <br />
+                            </Fragment>
+                        )) }
+                    </div>
+                    <div className="tools-container flex">
+                        <DeleteIco
+                            className={ `footer-icon ${isClrPickerOpen ? 'inactive' : ''}` }
+                            onClick={ (this.props.currLabel === 'garbage')
+                                ? () => this.props.handelNoteChange(note, 'title', 'garbage')
+                                : () => this.setState({ deleteModal: true })
+                            }
+                        />
+                        { this.state.deleteModal && <ModalDelete /> }
+                        <ClrPiker
+                            setBgClr={ this.setBgClr }
+                            currBgClr={ this.state.bgClr }
+                            clrPikerOpen={ this.ClrPikerOpen }
+                        />
+                        <AddImgIco className={ `footer-icon ${isClrPickerOpen ? 'inactive' : ''}` } />
+                    </div>
                 </div>
-                <div className={ "body" }>
-                    { note.body.split("\n").map((line, index) => (
-                        <Fragment key={ index }>
-                            { line }
-                            <br />
-                        </Fragment>
-                    )) }
-                </div>
-                <div className="tools-container flex">
-                    <DeleteIco
-                        className={ `footer-icon ${isClrPickerOpen ? 'inactive' : ''}` }
-                        onClick={ (this.props.currLabel === 'garbage')
-                            ? () => this.props.handelNoteChange(note, 'title', 'garbage')
-                            : () => this.setState({ deleteModal: true })
-                        }
-                    />
-                    { this.state.deleteModal && <ModalDelete /> }
-                    <ClrPiker
-                        setBgClr={ this.setBgClr }
-                        currBgClr={ this.state.bgClr }
-                        clrPikerOpen={ this.ClrPikerOpen }
-                    />
-                    <AddImgIco className={ `footer-icon ${isClrPickerOpen ? 'inactive' : ''}` } />
-                </div>
-            </div>
-        </section>
-    )
-}
+            </section>
+        )
+    }
 }
 
 export default WithNoteHandling(Notes)
